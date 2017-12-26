565a389497989ebb3b0ffe40c8cc07f964208f9a
lytsaber <harrison.tuja@strathmore.edu>
2017-12-26 10:55:25 +0300

    construction
    Construction

--- js/main.js
+++ js/main.js
@@ -1,85 +1,113 @@
-/* 
+jQuery(function($) {'use strict';
 
-Template 2089 Meteor
+	//Responsive Nav
+	$('li.dropdown').find('.fa-angle-down').each(function(){
+		$(this).on('click', function(){
+			if( $(window).width() < 768 ) {
+				$(this).parent().next().slideToggle();
+			}
+			return false;
+		});
+	});
 
-http://www.tooplate.com/view/2089-meteor
+	//Fit Vids
+	if( $('#video-container').length ) {
+		$("#video-container").fitVids();
+	}
 
-*/
+	//Initiat WOW JS
+	new WOW().init();
 
-jQuery(document).ready(function($) {
+	// portfolio filter
+	$(window).load(function(){
 
-	'use strict';
+		$('.main-slider').addClass('animate-in');
+		$('.preloader').remove();
+		//End Preloader
 
+		if( $('.masonery_area').length ) {
+			$('.masonery_area').masonry();//Masonry
+		}
 
-        $('.counter').each(function() {
-          var $this = $(this),
-              countTo = $this.attr('data-count');
-          
-          $({ countNum: $this.text()}).animate({
-            countNum: countTo
-          },
-
-          {
-
-            duration: 8000,
-            easing:'linear',
-            step: function() {
-              $this.text(Math.floor(this.countNum));
-            },
-            complete: function() {
-              $this.text(this.countNum);
-              //alert('finished');
-            }
-
-          });  
-          
-        });
-
-
-
-        $(".b1").click(function () {
-            $(".pop").fadeIn(300);
-            
-        });
+		var $portfolio_selectors = $('.portfolio-filter >li>a');
 		
-		$(".b2").click(function () {
-            $(".pop2").fadeIn(300);
-            
-        });
+		if($portfolio_selectors.length) {
+			
+			var $portfolio = $('.portfolio-items');
+			$portfolio.isotope({
+				itemSelector : '.portfolio-item',
+				layoutMode : 'fitRows'
+			});
+			
+			$portfolio_selectors.on('click', function(){
+				$portfolio_selectors.removeClass('active');
+				$(this).addClass('active');
+				var selector = $(this).attr('data-filter');
+				$portfolio.isotope({ filter: selector });
+				return false;
+			});
+		}
+
+	});
+
+
+	$('.timer').each(count);
+	function count(options) {
+		var $this = $(this);
+		options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+		$this.countTo(options);
+	}
 		
-		$(".b3").click(function () {
-            $(".pop3").fadeIn(300);
-            
-        });
+	// Search
+	$('.fa-search').on('click', function() {
+		$('.field-toggle').fadeToggle(200);
+	});
 
-        $(".pop > span, .pop").click(function () {
-            $(".pop").fadeOut(300);
-        });
-		
-		$(".pop2 > span, .pop2").click(function () {
-            $(".pop2").fadeOut(300);
-        });
-		
-		$(".pop3 > span, .pop3").click(function () {
-            $(".pop3").fadeOut(300);
-        });
+	// Contact form
+	var form = $('#main-contact-form');
+	form.submit(function(event){
+		event.preventDefault();
+		var form_status = $('<div class="form_status"></div>');
+		$.ajax({
+			url: $(this).attr('action'),
+			beforeSend: function(){
+				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
+			}
+		}).done(function(data){
+			form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
+		});
+	});
 
+	// Progress Bar
+	$.each($('div.progress-bar'),function(){
+		$(this).css('width', $(this).attr('data-transition')+'%');
+	});
 
-        $(window).on("scroll", function() {
-            if($(window).scrollTop() > 100) {
-                $(".header").addClass("active");
-            } else {
-                //remove the background property so it comes transparent again (defined in your css)
-               $(".header").removeClass("active");
-            }
-        });
+	if( $('#gmap').length ) {
+		var map;
 
+		map = new GMaps({
+			el: '#gmap',
+			lat: 43.04446,
+			lng: -76.130791,
+			scrollwheel:false,
+			zoom: 16,
+			zoomControl : false,
+			panControl : false,
+			streetViewControl : false,
+			mapTypeControl: false,
+			overviewMapControl: false,
+			clickable: false
+		});
 
-	/************** Mixitup (Filter Projects) *********************/
-    	$('.projects-holder').mixitup({
-            effects: ['fade','grayscale'],
-            easing: 'snap',
-            transitionSpeed: 400
-        });
+		map.addMarker({
+			lat: 43.04446,
+			lng: -76.130791,
+			animation: google.maps.Animation.DROP,
+			verticalAlign: 'bottom',
+			horizontalAlign: 'center',
+			backgroundColor: '#3e8bff',
+		});
+	}
 
 });
